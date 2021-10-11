a0a329f701ad81d82f5dfe874ab412df2b99382b
joel <joel.haubold@logarithmo.de>
2021-10-11 14:18:32 +0200

    Added to gitignore

--- load_data.py
+++ load_data.py
@@ -8,62 +8,63 @@ from torch.utils.data.sampler import SubsetRandomSampler
 import torch.optim as optim
 from net import Net
 
+if __name__ == '__main__':
+    train_dir = './data/train_images'
+    test_dir = './data/test_images'
+    real_dir = './data/real_images'
 
-train_dir = './train_images'
-test_dir = './test_images'
-real_dir = './real_images'
+    transform = transforms.Compose(
+        [transforms.Grayscale(),
+         transforms.ToTensor(),
+         transforms.Normalize(mean=(0,),std=(1,))])
 
-transform = transforms.Compose(
-    [transforms.Grayscale(), 
-     transforms.ToTensor(), 
-     transforms.Normalize(mean=(0,),std=(1,))])
+    train_data = torchvision.datasets.ImageFolder(train_dir, transform=transform)
+    test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
 
-train_data = torchvision.datasets.ImageFolder(train_dir, transform=transform)
-test_data = torchvision.datasets.ImageFolder(test_dir, transform=transform)
+    valid_size = 0.3
+    batch_size = 32
+    net = Net()
+    optimizer = optim.SGD(net.parameters(), lr=0.01)
+    criterion = nn.CrossEntropyLoss()
 
-valid_size = 0.3
-batch_size = 32
-net = Net()
-optimizer = optim.SGD(net.parameters(), lr=0.01)
-criterion = nn.CrossEntropyLoss()
+    num_train = len(train_data)
+    indices_train = list(range(num_train))
+    np.random.shuffle(indices_train)
+    split_tv = int(np.floor(valid_size * num_train))
+    train_new_idx, valid_idx = indices_train[split_tv:],indices_train[:split_tv]
 
-num_train = len(train_data)
-indices_train = list(range(num_train))
-np.random.shuffle(indices_train)
-split_tv = int(np.floor(valid_size * num_train))
-train_new_idx, valid_idx = indices_train[split_tv:],indices_train[:split_tv]
+    train_sampler = SubsetRandomSampler(train_new_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
 
-train_sampler = SubsetRandomSampler(train_new_idx)
-valid_sampler = SubsetRandomSampler(valid_idx)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=train_sampler, num_workers=1)
+    valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler, num_workers=1)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=1)
+    classes = ('noface','face')
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=train_sampler, num_workers=1)
-valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler, num_workers=1)
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True, num_workers=1)
-classes = ('noface','face')
+    n_epochs = 10
 
-n_epochs = 1
+    for epoch in range(1, n_epochs+1):
+        print(f"nmbr epoch: {epoch}")
+        for data in train_loader:
+            optimizer.zero_grad()
+            images, labels = data
+            outputs = net(images)
+            #_, predicted = torch.max(outputs.data, 1)
+            #predicted = predicted.type(torch.float)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-for epoch in range(1, n_epochs+1):
-    for data in train_loader:
-        optimizer.zero_grad()
-        images, labels = data
-        outputs = net(images) # This has an extra dimension, why? ([32, 2])
-        #_, predicted = torch.max(outputs.data, 1)
-        #predicted = predicted.type(torch.float)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in test_loader:
-        images, labels = data
-        outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
 
-print('Accuracy of the network on the 10000 on dummy faces  images: %d %%' % (
-    100 * correct / total))
+    print('Accuracy of the network on the 10000 on dummy faces  images: %d %%' % (
+        100 * correct / total))
